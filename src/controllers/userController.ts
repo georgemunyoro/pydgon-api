78e764b7005d3d6090ceebaa1f40860b368b82cc
George Munyoro <george.guva@outlook.com>
2020-11-11 16:22:28 +0200

    Remove placeholder user controllers

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -30,33 +30,3 @@ export async function userDetail(req: express.Request, res: express.Response) {
     },
   });
 }
-
-// Display User create form on GET.
-export async function userCreateGet(req, res) {
-  res.send("NOT IMPLEMENTED: User create GET");
-}
-
-// Handle User create on POST.
-export async function userCreatePost(req, res) {
-  res.send("NOT IMPLEMENTED: User create POST");
-}
-
-// Display User delete form on GET.
-export async function userDeleteGet(req, res) {
-  res.send("NOT IMPLEMENTED: User delete GET");
-}
-
-// Handle User delete on POST.
-export async function userDeletePost(req, res) {
-  res.send("NOT IMPLEMENTED: User delete POST");
-}
-
-// Display User update form on GET.
-export async function userUpdateGet(req, res) {
-  res.send("NOT IMPLEMENTED: User update GET");
-}
-
-// Handle User update on POST.
-export async function userUpdatePost(req, res) {
-  res.send("NOT IMPLEMENTED: User update POST");
-}
