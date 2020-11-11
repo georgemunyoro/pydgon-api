cd987455f9167aa6db762add29efca4265699659
George Munyoro <george.guva@outlook.com>
2020-11-11 14:57:46 +0200

    Updat to use sequelize ORM instead of node pg

--- src/database.ts
+++ src/database.ts
@@ -1,27 +1,4 @@
-import { Pool, Client } from 'pg';
+import { Sequelize } from 'sequelize';
+import databaseConfig from '../config/database.json';
 
-export function getDatabaseConnection(): Promise<Client> {
-    const promise: Promise<Client> = new Promise((resolve, reject) => {
-        try {
-            const client = new Client();
-            client.connect();
-            resolve(client);
-        } catch (error) {
-            reject(error);
-        }
-    });
-    return promise;
-}
-
-export function query(databaseConnection: Client, query: string): Promise<any> {
-    const promise: Promise<any> = new Promise(async (resolve, reject) => {
-        try {
-            const res = await databaseConnection.query(query);
-            await databaseConnection.end();
-            resolve(200);
-        } catch (error) {
-            reject(error);
-        }
-    });
-    return promise;
-}
+export const sequelize = new Sequelize(databaseConfig.connectionString);
