0b4ad57887677ec55fc53bc4bc20e3508d426b58
George Munyoro <george.guva@outlook.com>
2020-11-12 16:50:17 +0200

    Implement message list controller and delete unimplemented placeholder controllers

--- src/controllers/messageController.ts
+++ src/controllers/messageController.ts
@@ -1,41 +1,58 @@
+import * as express from "express";
+import { Op } from "sequelize";
 import { Message } from "../models/message";
+import { getRequestAuthUser } from "./authController";
 
 // Display list of all Message.
-export async function messageList(req, res) {
-  res.send("NOT IMPLEMENTED: Message list");
+export async function messageList(req: express.Request, res: express.Response) {
+  try {
+    const [err, data] = await getRequestAuthUser(req);
+
+    if (err) {
+      return res.status(403).json({
+        message: "Unauthorized",
+        data: {
+          errors: [data.message],
+        },
+      });
+    }
+
+    if (req.params.userId === 'undefined') {
+      return res.status(404).json({
+        message: "Missing required parameters",
+        data: {
+          errors: ["uuid is undefined"]
+        }
+      });
+    }
+
+    const messages = await Message.findAll({
+      where: {
+        [Op.or]: [
+          { sender: data.authenticatedUser.uuid, recepient: req.params.userId },
+          { sender: req.params.userId, recepient: data.authenticatedUser.uuid },
+        ],
+      },
+    });
+
+    return res.status(200).json({
+      message: "Messages",
+      data: {
+        messages: messages,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      message: "An error ocurred",
+      data: {
+        error,
+      },
+    });
+  }
 }
 
 // Display detail page for a specific Message.
 export async function messageDetail(req, res) {
   res.send("NOT IMPLEMENTED: Message detail: " + req.params.id);
-}
-
-// Display Message create form on GET.
-export async function messageCreateGet(req, res) {
-  res.send("NOT IMPLEMENTED: Message create GET");
-}
-
-// Handle Message create on POST.
-export async function messageCreatePost(req, res) {
-  res.send("NOT IMPLEMENTED: Message create POST");
-}
-
-// Display Message delete form on GET.
-export async function messageDeleteGet(req, res) {
-  res.send("NOT IMPLEMENTED: Message delete GET");
-}
-
-// Handle Message delete on POST.
-export async function messageDeletePost(req, res) {
-  res.send("NOT IMPLEMENTED: Message delete POST");
-}
-
-// Display Message update form on GET.
-export async function messageUpdateGet(req, res) {
-  res.send("NOT IMPLEMENTED: Message update GET");
-}
-
-// Handle Message update on POST.
-export async function messageUpdatePost(req, res) {
-  res.send("NOT IMPLEMENTED: Message update POST");
 }
