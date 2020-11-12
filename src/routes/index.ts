e6a368df875dfe0cf2ebc97e4caaa5bb2b992491
George Munyoro <george.guva@outlook.com>
2020-11-12 16:54:26 +0200

    Add messages and contacts endpoints to router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,11 +3,15 @@ import { Router } from "express";
 
 import { authRouter } from "./auth";
 import { userRouter } from "./users";
+import { messageRouter } from "./messages";
+import { contactRouter } from './contacts';
 
 export const router = Router();
 
 router.use("/auth", authRouter);
 router.use("/users", userRouter);
+router.use("/messages", messageRouter);
+router.use("/contacts", contactRouter);
 
 router.get("/", (req: any, res: any) => {
   res.status(200).json({
