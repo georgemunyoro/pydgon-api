4539f8abfbac203c2a6ff797de939b43e68b51e9
George Munyoro <george.guva@outlook.com>
2020-11-12 16:50:49 +0200

    Add promise rejection handling

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -3,30 +3,50 @@ import { User } from "../models/user";
 
 // Display list of all Users
 export async function userList(req, res) {
-  const users = await User.findAll({
-    attributes: ["username", "uuid"],
-  });
+  try {
+    const users = await User.findAll({
+      attributes: ["username", "uuid"],
+    });
 
-  res.status(200).json({
-    message: "User List",
-    data: {
-      users,
-    },
-  });
+    res.status(200).json({
+      message: "User List",
+      data: {
+        users,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      message: "An error ocurred",
+      data: {
+        error,
+      },
+    });
+  }
 }
 
 // Display detail page for a specific User.
 export async function userDetail(req: express.Request, res: express.Response) {
-  const uuid = req.params.userId;
-  const user = await User.findOne({
-    where: { uuid },
-    attributes: { exclude: ["email", "password"] },
-  });
+  try {
+    const uuid = req.params.userId;
+    const user = await User.findOne({
+      where: { uuid },
+      attributes: { exclude: ["email", "password"] },
+    });
 
-  return res.status(200).json({
-    message: "User Detail",
-    data: {
-      user,
-    },
-  });
+    return res.status(200).json({
+      message: "User Detail",
+      data: {
+        user,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      message: "An error has ocurred",
+      data: {
+        error,
+      },
+    });
+  }
 }
