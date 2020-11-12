9c3fb2883616bf84185a0a661a2e4706dc60013d
George Munyoro <george.guva@outlook.com>
2020-11-12 16:53:26 +0200

    Create route for contacts

---
+++ src/routes/contacts.ts
@@ -0,0 +1,7 @@
+import * as express from 'express';
+import * as contacts from '../controllers/contactController';
+
+export const contactRouter = express.Router();
+
+contactRouter.get('/', contacts.contactList);
+contactRouter.post('/:uuid', contacts.createContactPost);
