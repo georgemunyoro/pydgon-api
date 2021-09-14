3da16ee573d6c9e0864bcb4bef77cb86f1646bb5
George Munyoro <george.guva@outlook.com>
2021-09-14 17:56:18 +0200

    chore: move v1 source files to v1 folder

--- index.ts
+++ index.ts
@@ -1,14 +1,13 @@
-import {Socket} from 'dgram';
-import express from 'express';
-import {router} from './src/routes';
 import cors from 'cors';
+import {Socket} from 'dgram';
+import * as dotenv from 'dotenv';
+import express from 'express';
 import logger from 'morgan';
 
-import {Message} from './src/models/message';
-import {Contact} from './src/models/contact';
-import {User} from './src/models/user';
-
-import * as dotenv from 'dotenv';
+import {Contact} from './v1/models/contact';
+import {Message} from './v1/models/message';
+import {User} from './v1/models/user';
+import {router} from './v1/routes';
 
 dotenv.config();
 
@@ -23,68 +22,68 @@ app.use(logger('dev'));
 app.use('/api/v1/', router);
 
 io.on('connection', (socket: Socket) => {
-  socket.on('user-online', (user: any) => {
-    io.sockets.emit('user-online', user);
-  });
-
-  socket.on('user-offline', (user: any) => {
-    io.sockets.emit('user-offline', user);
-  });
-
-  socket.on('status-check', (userId: string) => {
-    io.sockets.emit(userId + '-status-check');
-  });
-
-  socket.on('message-read', async (msg: any) => {
-    try {
-      let message = await Message.findOne({
-        where: {
-          id: msg.id,
-        },
-      });
-
-      if (message) {
-        console.log('MEG', message.getDataValue('id'));
-        message.setDataValue('read', true);
-        await message.save();
-        io.sockets.emit(msg.id + '-read', message);
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  });
-
-  socket.on('chat-message', async (msg: any) => {
-    console.log(msg);
-    const message = await Message.create(msg);
-    const userHasContact = await Contact.findOne({
-      where: {
-        user: msg.recepient,
-        contact: msg.sender,
-      },
+    socket.on('user-online', (user: any) => {
+        io.sockets.emit('user-online', user);
     });
 
-    if (!userHasContact) {
-      const sender = await User.findOne({
-        where: {
-          uuid: msg.sender,
-        },
-      });
+    socket.on('user-offline', (user: any) => {
+        io.sockets.emit('user-offline', user);
+    });
 
-      Contact.create({
-        user: msg.recepient,
-        contact: msg.sender,
-        name: sender?.getDataValue('username'),
-      });
-    }
+    socket.on('status-check', (userId: string) => {
+        io.sockets.emit(userId + '-status-check');
+    });
 
-    io.sockets.emit(`${msg.recepient}-new-message`, message);
-  });
+    socket.on('message-read', async (msg: any) => {
+        try {
+            let message = await Message.findOne({
+                where: {
+                    id: msg.id,
+                },
+            });
+
+            if (message) {
+                console.log('MEG', message.getDataValue('id'));
+                message.setDataValue('read', true);
+                await message.save();
+                io.sockets.emit(msg.id + '-read', message);
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    });
+
+    socket.on('chat-message', async (msg: any) => {
+        console.log(msg);
+        const message = await Message.create(msg);
+        const userHasContact = await Contact.findOne({
+            where: {
+                user: msg.recepient,
+                contact: msg.sender,
+            },
+        });
+
+        if (!userHasContact) {
+            const sender = await User.findOne({
+                where: {
+                    uuid: msg.sender,
+                },
+            });
+
+            Contact.create({
+                user: msg.recepient,
+                contact: msg.sender,
+                name: sender?.getDataValue('username'),
+            });
+        }
+
+        io.sockets.emit(`${msg.recepient}-new-message`, message);
+    });
 });
 
-const port: Number | string = process.env.PORT || 5000;
+const port: Number|string = process.env.PORT || 5000;
 http.listen(port, () => {
-  console.log('Server is started at http://localhost:' + port);
+    console.log('Server is started at http://localhost:' + port);
 });
 
 export default app;
